1eeccaabc9f4e45fc5d850d4ad9c5b292a7dc1c7
nararyacodetechy <bagusnararyanandaraditya@gmail.com>
2022-12-10 17:45:43 +0800

    Add Page Category

---
+++ pasti_ada/lib/category/fashion_category.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FashionCategoryPage extends StatelessWidget {
+  const FashionCategoryPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
